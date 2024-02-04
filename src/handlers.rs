908179f02969175ed37524592a72bc786b8bf888
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2024-02-04 12:51:13 +0100

    refact: move handlers to separate file

---
+++ src/handlers.rs
@@ -0,0 +1,104 @@
+use actix_web::{delete, get, patch, post, web, HttpResponse};
+use maud::{html, Markup};
+
+use crate::error::ApiResult;
+use crate::repository::{Repository, Todo};
+use crate::views;
+
+#[get("/")]
+pub async fn index(db: web::Data<Repository>) -> ApiResult<Markup> {
+    let todos = db.get_all().await?;
+
+    let title = "Todo list";
+    let content = html! {
+        div class="container mx-auto" {
+            div class="max-w-md mx-auto my-10 bg-slate-100 p-5 rounded-lg"{
+                h1 class="text-2xl font-bold text-center" {
+                    "Items"
+                }
+                div class="p-5 flex flex-col gap-4" {
+                    (views::todos_view(todos))
+                    (views::delete_done_view())
+                    (views::add_todo_view())
+                }
+            }
+        }
+    };
+
+    Ok(views::index_view(title, content))
+}
+
+#[derive(serde::Deserialize)]
+pub struct AddForm {
+    text: String,
+}
+
+#[post("/todos")]
+async fn add_new(db: web::Data<Repository>, form: web::Form<AddForm>) -> ApiResult<Markup> {
+    let todo = db.insert(form.into_inner().text).await?;
+
+    Ok(views::todo_view(todo))
+}
+
+#[post("/todos/{id}/toggle_done")]
+async fn toggle_done(db: web::Data<Repository>, path: web::Path<i64>) -> ApiResult<Markup> {
+    let id = path.into_inner();
+    let todo = db.get_by_id(id).await?;
+
+    let todo = Todo {
+        is_done: !todo.is_done,
+        ..todo
+    };
+    let todo = db.update(todo).await?;
+
+    Ok(views::todo_view(todo))
+}
+
+#[get("/todos/{id}/edit")]
+async fn start_edit(db: web::Data<Repository>, path: web::Path<i64>) -> ApiResult<Markup> {
+    let id = path.into_inner();
+    let todo = db.get_by_id(id).await?;
+
+    Ok(views::edit_todo_view(todo))
+}
+
+#[derive(serde::Deserialize)]
+pub struct UpdateForm {
+    text: String,
+}
+
+#[patch("/todos/{id}")]
+async fn update(
+    db: web::Data<Repository>,
+    path: web::Path<i64>,
+    form: web::Form<UpdateForm>,
+) -> ApiResult<Markup> {
+    let id = path.into_inner();
+    let todo = db.get_by_id(id).await?;
+
+    let todo = Todo {
+        text: form.into_inner().text,
+        ..todo
+    };
+    let todo = db.update(todo).await?;
+
+    Ok(views::todo_view(todo))
+}
+
+#[delete("/todos/{id}")]
+async fn delete(db: web::Data<Repository>, path: web::Path<i64>) -> ApiResult<HttpResponse> {
+    let id = path.into_inner();
+
+    db.delete(id).await?;
+
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[delete("/todos")]
+async fn delete_all_done(db: web::Data<Repository>) -> ApiResult<Markup> {
+    db.delete_all_done().await?;
+
+    let todos = db.get_all().await?;
+
+    Ok(views::todos_view(todos))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,11 @@
-use actix_web::{delete, get, patch, post, web, App, HttpResponse, HttpServer};
-use maud::{html, Markup};
+use actix_web::{web, App, HttpServer};
 
-use crate::error::ApiResult;
-use crate::repository::{Repository, Todo};
+use crate::repository::Repository;
 
 mod error;
+mod handlers;
 mod repository;
 mod views;
-
-#[get("/")]
-pub async fn index(db: web::Data<Repository>) -> ApiResult<Markup> {
-    let todos = db.get_all().await?;
-
-    let title = "Todo list";
-    let content = html! {
-        div class="container mx-auto" {
-            div class="max-w-md mx-auto my-10 bg-slate-100 p-5 rounded-lg"{
-                h1 class="text-2xl font-bold text-center" {
-                    "Items"
-                }
-                div class="p-5 flex flex-col gap-4" {
-                    (views::todos_view(todos))
-                    (views::delete_done_view())
-                    (views::add_todo_view())
-                }
-            }
-        }
-    };
-
-    Ok(views::index_view(title, content))
-}
-
-#[derive(serde::Deserialize)]
-pub struct AddForm {
-    text: String,
-}
-
-#[post("/todos")]
-async fn add_new(db: web::Data<Repository>, form: web::Form<AddForm>) -> ApiResult<Markup> {
-    let todo = db.insert(form.into_inner().text).await?;
-
-    Ok(views::todo_view(todo))
-}
-
-#[post("/todos/{id}/toggle_done")]
-async fn toggle_done(db: web::Data<Repository>, path: web::Path<i64>) -> ApiResult<Markup> {
-    let id = path.into_inner();
-    let todo = db.get_by_id(id).await?;
-
-    let todo = Todo {
-        is_done: !todo.is_done,
-        ..todo
-    };
-    let todo = db.update(todo).await?;
-
-    Ok(views::todo_view(todo))
-}
-
-#[get("/todos/{id}/edit")]
-async fn start_edit(db: web::Data<Repository>, path: web::Path<i64>) -> ApiResult<Markup> {
-    let id = path.into_inner();
-    let todo = db.get_by_id(id).await?;
-
-    Ok(views::edit_todo_view(todo))
-}
-
-#[derive(serde::Deserialize)]
-pub struct UpdateForm {
-    text: String,
-}
-
-#[patch("/todos/{id}")]
-async fn update(
-    db: web::Data<Repository>,
-    path: web::Path<i64>,
-    form: web::Form<UpdateForm>,
-) -> ApiResult<Markup> {
-    let id = path.into_inner();
-    let todo = db.get_by_id(id).await?;
-
-    let todo = Todo {
-        text: form.into_inner().text,
-        ..todo
-    };
-    let todo = db.update(todo).await?;
-
-    Ok(views::todo_view(todo))
-}
-
-#[delete("/todos/{id}")]
-async fn delete(db: web::Data<Repository>, path: web::Path<i64>) -> ApiResult<HttpResponse> {
-    let id = path.into_inner();
-
-    db.delete(id).await?;
-
-    Ok(HttpResponse::Ok().finish())
-}
-
-#[delete("/todos")]
-async fn delete_all_done(db: web::Data<Repository>) -> ApiResult<Markup> {
-    db.delete_all_done().await?;
-
-    let todos = db.get_all().await?;
-
-    Ok(views::todos_view(todos))
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -115,13 +16,13 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(database.clone()))
-            .service(index)
-            .service(add_new)
-            .service(toggle_done)
-            .service(start_edit)
-            .service(update)
-            .service(delete)
-            .service(delete_all_done)
+            .service(handlers::index)
+            .service(handlers::add_new)
+            .service(handlers::toggle_done)
+            .service(handlers::start_edit)
+            .service(handlers::update)
+            .service(handlers::delete)
+            .service(handlers::delete_all_done)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
